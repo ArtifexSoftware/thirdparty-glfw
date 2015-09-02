bdc3c93f40279e445317ebf96417af35a85883b4
Camilla Berglund <elmindreda@elmindreda.org>
2015-09-02 13:42:28 +0200

    Fix full screen on dual-GPU MacBooks with OS X
    When automatic graphics switching is activated, the CGDirectDisplayID
    for the display changes, casuing the primary GLFWmonitor to disappear.

--- src/cocoa_monitor.m
+++ src/cocoa_monitor.m
@@ -285,7 +285,8 @@ _GLFWmonitor** _glfwPlatformGetMonitors(int* count)
         char* name = getDisplayName(displays[i]);
 
         monitor = _glfwAllocMonitor(name, size.width, size.height);
-        monitor->ns.displayID = displays[i];
+        monitor->ns.displayID  = displays[i];
+        monitor->ns.unitNumber = CGDisplayUnitNumber(displays[i]);
 
         free(name);
 
@@ -301,7 +302,7 @@ _GLFWmonitor** _glfwPlatformGetMonitors(int* count)
 
 GLboolean _glfwPlatformIsSameMonitor(_GLFWmonitor* first, _GLFWmonitor* second)
 {
-    return first->ns.displayID == second->ns.displayID;
+    return first->ns.unitNumber == second->ns.unitNumber;
 }
 
 void _glfwPlatformGetMonitorPos(_GLFWmonitor* monitor, int* xpos, int* ypos)
